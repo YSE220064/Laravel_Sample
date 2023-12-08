3ef7bb4609fb2d3025cbc2eb8dbf03ea23f8b5d6
220064@YSE <220064md@yse-c.net>
2023-12-08 11:10:52 +0900

    include comp err

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -2,7 +2,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+            
+            @include('components.error')
+            
+            <div class="p-6 text-gray-900">
                     <h2>商品編集</h2>
                     <form action="{{ route('item.update', $item->id) }}" method="post">
                         @csrf
